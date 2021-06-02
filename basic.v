10b2e974a237a4c8b72e6f2413ec90efc064c1ee
MisakaCenter <misaka@sjtu.edu.cn>
2021-06-02 23:44:32 +0800

    feat: opg matrix generation in coq.

--- basic.v
+++ basic.v
@@ -86,8 +86,10 @@ Fixpoint find_NT (target: grammar): list string :=
 (* x not in find_NT iff. x is T *)
 Definition find_T (target: grammar): list string := filter (fun x => negb (In_string (find_NT target) x)) (find_string_in_grammar target).
 
+(*
 Compute (find_NT test). (* = ["E"; "T"; "F"] : list string *)
 Compute (find_T test). (* = ["+"; "*"; "("; ")"; "i"] : list string *)
+*)
 
 Definition reify_symbol (nt: list string)(s: symbol): symbol :=
     let str:= get_string_from_symbol s in
@@ -109,7 +111,7 @@ Definition reify t:= _reify (find_NT t) t.
 
 Definition test_reify := reify test.
 
-Compute test_reify. 
+(* Compute test_reify.  *)
 (* = [(NT "E", combine (base [NT "E"; T "+"; NT "T"]) (base [NT "T"]));
       (NT "T", combine (base [NT "T"; T "*"; NT "F"]) (base [NT "F"]));
       (NT "F", combine (base [T "("; NT "E"; T ")"]) (base [T "i"]))]
@@ -204,12 +206,203 @@ Definition lastvt (g: grammar):=
     let rev_g:= map (fun x => (fst x, rev_expr (snd x))) g in
         firstvt rev_g.
 
-Compute (firstvt test_reify).
+(* Compute (firstvt test_reify). *)
 (* = [["E"; "T"; "F"]; ["+"; "*"; "("; "i"]; ["*"; "("; "i"]; ["("; "i"]]
 : list (list string) *)
-Compute (lastvt test_reify).
+(* Compute (lastvt test_reify). *)
 (* = [["E"; "T"; "F"]; ["+"; "*"; ")"; "i"]; ["*"; ")"; "i"]; [")"; "i"]]
 : list (list string) *)
 
 End FIRSTVT_and_LASTVT.
 
+Section OPG_Matrix.
+
+Inductive relation: Type :=
+| le : relation (* < *)
+| ge : relation (* > *)
+| eq : relation (* = *)
+| emp : relation. (* ? undecided *)
+
+Definition matrix: Type := list (list relation).
+
+Definition is_emp (r: relation): bool :=
+    match r with
+    | emp => true
+    | _ => false
+    end.
+
+Fixpoint list_n_times {T: Type}(x: T)(n: nat): list T :=
+    match n with
+    | S n => x :: (list_n_times x n)
+    | O => nil
+    end.
+
+Definition matrix_gen_emp (g: grammar): matrix :=
+    let len:= length (find_T g) in
+        let row:= list_n_times emp len in
+            list_n_times row len.
+
+Definition default_relation: list relation := nil.
+
+Fixpoint change_m_to_x (l: list relation)(m: nat)(x: relation): list relation :=
+    match l with
+    | nil => nil
+    | x'::xs => match m with
+                | 0 => x::xs
+                | S m => x'::(change_m_to_x xs m x)
+                end
+    end.
+
+Fixpoint change_n_m_to_x (mtx: matrix)(n m:nat)(x: relation): matrix :=
+    match mtx with
+    | nil => nil
+    | x'::xs => match n with
+                | 0 => (change_m_to_x x' m x)::xs
+                | S n => x'::(change_n_m_to_x xs n m x)
+                end
+    end.
+
+Definition is_T (s: symbol): bool :=
+    match s with
+    | T _ => true
+    | _ => false
+    end.
+
+Definition is_NT (s: symbol): bool :=
+    match s with
+    | NT _ => true
+    | _ => false
+    end.
+
+Fixpoint _find_x_in_g (l: list string)(x: string): nat :=
+    match l with
+    | x'::xs => if string_dec x' x then O else S (_find_x_in_g xs x)
+    | nil => O
+    end.
+
+Definition find_x_in_g_symbol (g: grammar)(x: symbol) := _find_x_in_g (find_T g) (get_string_from_symbol x).
+Definition find_x_in_g_str (g: grammar)(x: string) := _find_x_in_g (find_T g) x.
+
+(* find => aQb | ab => a equal b*)
+Fixpoint equal_list_eval (m: matrix)(l: list symbol)(g: grammar): matrix :=
+    match l with
+    | nil => m
+    | x::xs => if is_T x 
+                    then (let (snd, trd):= (nth_default default xs 0, nth_default default xs 1) in
+                            if (andb (is_NT snd) (is_T trd)) (* aQb *)
+                                then (let (a, b):= (find_x_in_g_symbol g x, find_x_in_g_symbol g trd) in 
+                                        equal_list_eval (change_n_m_to_x m a b eq) xs g)
+                                else if is_T snd 
+                                        then (let (a, b):= (find_x_in_g_symbol g x, find_x_in_g_symbol g snd) in 
+                                                equal_list_eval (change_n_m_to_x m a b eq) xs g)
+                                        else equal_list_eval m xs g)
+                    else equal_list_eval m xs g
+    end.
+
+Fixpoint equal_expr_eval (m: matrix)(expr: expr)(g: grammar): matrix :=
+    match expr with
+    | base l => equal_list_eval m l g
+    | combine a b => equal_expr_eval (equal_expr_eval m a g) b g
+    end.
+
+Fixpoint _equal_eval (m: matrix)(exprs: list expr)(g: grammar): matrix :=
+    match exprs with
+    | nil => m
+    | x::xs => _equal_eval (equal_expr_eval m x g) xs g
+    end.
+
+Definition firstvt_gen (g: grammar)(nt: symbol): list string := 
+    let fst:= firstvt g in
+        nth_default nil fst (_find_x_in_g (nth_default nil fst 0) (get_string_from_symbol nt) + 1).
+
+Fixpoint fold_le (g: grammar)(l: list string)(m: matrix)(left: nat): matrix :=
+    match l with
+    | nil => m
+    | x::xs => let a:= find_x_in_g_str g x in 
+                    fold_le g xs (change_n_m_to_x m left a le) left
+    end.
+
+(* find => aQ => a < firstvt(Q)*)
+Fixpoint less_list_eval (m: matrix)(l: list symbol)(g: grammar): matrix :=
+    match l with
+    | nil => m
+    | x::xs => if is_T x 
+                    then (let snd:= nth_default default xs 0 in
+                            if (is_NT snd) (* aQ *)
+                                then (let fstq:= firstvt_gen g snd in 
+                                        less_list_eval (fold_le g fstq m (find_x_in_g_symbol g x)) xs g)
+                                else less_list_eval m xs g)
+                    else less_list_eval m xs g
+    end.
+
+Definition lastvt_gen (g: grammar)(nt: symbol): list string := 
+    let fst:= lastvt g in
+        nth_default nil fst (_find_x_in_g (nth_default nil fst 0) (get_string_from_symbol nt) + 1).
+
+Fixpoint fold_ge (g: grammar)(l: list string)(m: matrix)(right: nat): matrix :=
+    match l with
+    | nil => m
+    | x::xs => let a:= find_x_in_g_str g x in 
+                    fold_ge g xs (change_n_m_to_x m a right ge) right
+    end.
+
+(* find => Qa => firstvt(Q) > a*)
+Fixpoint greater_list_eval (m: matrix)(l: list symbol)(g: grammar): matrix :=
+    match l with
+    | nil => m
+    | x::xs => if is_NT x 
+                    then (let snd:= nth_default default xs 0 in
+                            if (is_T snd) (* Qa *)
+                                then (let lstq:= lastvt_gen g x in 
+                                        greater_list_eval (fold_ge g lstq m (find_x_in_g_symbol g snd)) xs g)
+                                else greater_list_eval m xs g)
+                    else greater_list_eval m xs g
+    end.
+
+(* meta-programming! for less, greater and equal generation *)
+Fixpoint meta_expr_eval (f: matrix -> list symbol -> grammar -> matrix)(m: matrix)(e: expr)(g: grammar): matrix :=
+    match e with
+    | base l => f m l g
+    | combine a b => meta_expr_eval f (meta_expr_eval f m a g) b g
+    end.
+
+Fixpoint _meta_eval (f: matrix -> list symbol -> grammar -> matrix)(m: matrix)(exprs: list expr)(g: grammar): matrix :=
+    match exprs with
+    | nil => m
+    | x::xs => _meta_eval f (meta_expr_eval f m x g) xs g
+    end.
+
+Definition meta_eval (f: matrix -> list symbol -> grammar -> matrix)(g: grammar):=
+    _meta_eval f (matrix_gen_emp g) (map snd g) g.
+
+(* OPG matrix can be generated using following 3 functions *)
+Definition equal_eval (g: grammar):= meta_eval equal_list_eval g.
+Definition less_eval (g: grammar):= meta_eval less_list_eval g.
+Definition greater_eval (g: grammar):= meta_eval greater_list_eval g.
+
+(* merge three matrix into one *)
+Fixpoint merge_list (l1 l2: list relation): list relation :=
+    if Nat.eqb (length l1) (length l2) then
+        match l1, l2 with
+        | x1::xs1, x2::xs2 => if is_emp x1 then x2::(merge_list xs1 xs2)
+                                           else if is_emp x2 then x1::(merge_list xs1 xs2)
+                                                             else nil (* both are not empty, should not happen! *)
+        | _, _ => nil
+        end
+    else nil.
+
+Fixpoint merge (m1 m2: matrix): matrix :=
+    if Nat.eqb (length m1) (length m2) then
+        match m1, m2 with
+        | x1::xs1, x2::xs2 => (merge_list x1 x2)::(merge xs1 xs2)
+        | _, _ => nil
+        end
+    else nil.
+
+Definition get_opg_matrix (g: grammar) := merge (merge (less_eval g) (equal_eval g)) (greater_eval g).
+
+Compute test_reify.
+Compute find_T test_reify.
+Compute get_opg_matrix test_reify.
+
+End OPG_Matrix.
